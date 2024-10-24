db0a99391601b37b203c99c50c0e6aa419e79bf5
SKTT1Fiver <1585403748@qq.com>
2024-10-24 10:29:58 +0800

    init: 初始化通用后端项目

---
+++ src/main/java/org/example/commonback/CommonBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.commonback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonBackApplication.class, args);
+    }
+
+}
